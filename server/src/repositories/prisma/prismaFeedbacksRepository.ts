ee9705271ceafacb1822c81c221e9d26e428aee9
Thyago Carvalho <othyagocarvalho@outlook.com>
2022-05-07 22:43:04 -0300

    improve prisma feedback file sintax

--- server/src/repositories/prisma/prismaFeedbacksRepository.ts
+++ server/src/repositories/prisma/prismaFeedbacksRepository.ts
@@ -5,9 +5,9 @@ import { FeedbackCreateData, FeedbacksRepository } from "../feedbacksRepository"
 export class PrismaFeedbacksRepository implements FeedbacksRepository {
   async create({type, comment, screenshot}: FeedbackCreateData) {
     await prisma.feedback.create({ data: {
-      type: type,
-      comment: comment,
-      screenshot: screenshot
+      type,
+      comment,
+      screenshot,
     }});
   }
 }
